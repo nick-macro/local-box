eaf818143258a2139b70d44f21ee898c9e8b3e8f
Nick Macro <nmacro34@gmail.com>
2025-11-01 22:31:42 -0400

    switch to docker desktop

--- src/local_box/dependencies/docker.py
+++ src/local_box/dependencies/docker.py
@@ -3,5 +3,5 @@ from .base_classes.brew import BrewDependency
 class Docker(BrewDependency):
     def __init__(self) -> None:
         self.name = "docker"
-        self.cask = False
+        self.cask = True
         self.tap = None
