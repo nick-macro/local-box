3bbee8c81dcaf34557498614105233004fd99df1
Nick Macro <5282600+nick-macro@users.noreply.github.com>
2025-12-06 22:01:19 -0500

    remove gh from setup script
    repo is public so it's not needed at this stage

--- setup.sh
+++ setup.sh
@@ -1,10 +1,7 @@
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 eval "$(/opt/homebrew/bin/brew shellenv)"
 
-brew install git gh uv
-
-# login to GitHub using the website (-w), copy the device code to the clipboard (-c), and use SSH for authentication (-p ssh)
-gh auth login -w -c -p ssh
+brew install git uv
 
 mkdir -p $HOME/repos
 mkdir -p $HOME/.local/bin
