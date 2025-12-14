f597a02dd65581efe4017c2f9c7d4de742d57a23
Nick Macro <nmacro34@gmail.com>
2025-12-13 20:47:18 -0500

    update setup

--- setup.sh
+++ setup.sh
@@ -1,18 +1,35 @@
-echo "Installing homebrew..."
-/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
-eval "$(/opt/homebrew/bin/brew shellenv)"
 
-echo "Installing git and uv..."
-brew install git uv
+if command -v brew &> /dev/null; then
+    echo "✅ Homebrew detected. Skipping Homebrew installation."
+else
+    echo "🔧 Homebrew not detected. Installing Homebrew."
+    /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+    eval "$(/opt/homebrew/bin/brew shellenv)"
+fi
 
-echo "Making repos directory..."
-mkdir -p $HOME/repos
-mkdir -p $HOME/.local/bin
+if [ "$(which uv)" = "/opt/homebrew/bin/uv" ]; then
+    echo "✅ uv is installed via Homebrew. Skipping uv installation."
+else
+    echo "🔧 Homebrew installation of uv not detected. Installing uv via Homebrew."
+    brew install uv
+fi
 
-echo "Installing local-box..."
-cd $HOME/repos
-git clone git@github.com:nick-macro/local-box.git
-uv tool install --editable ./local-box
+REPO_PATH="$HOME/repos/local-box"
+REPO_URL="git@github.com:nick-macro/local-box.git"
 
-echo "Installing dependencies using local-box..."
+if [ -d "$REPO_PATH" ] && git -C "$REPO_PATH" rev-parse --git-dir &> /dev/null; then
+    echo "✅ Repository detected. Skipping clone."
+else
+    echo "🔧 Cloning repository."
+    git clone "$REPO_URL" "$REPO_PATH"
+fi
+
+if uv tool list | grep -q "^local-box "; then
+    echo "✅ local-box is installed. Skipping installation."
+else
+    echo "🔧 local-box is not installed as a uv tool"
+    uv tool install --editable $HOME/repos/local-box
+fi
+
+echo "🔧 Installing dependencies using local-box..."
 uvx local-box sync
